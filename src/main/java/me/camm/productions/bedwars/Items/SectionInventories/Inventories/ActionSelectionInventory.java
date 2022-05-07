6f60005358afa42a50ff91d5f32a93881bada357
Talon J <talon.jiang@ryerson.ca>
2022-05-06 21:49:04 -0400

    Fixed the compilation errors in the code.

--- src/main/java/me/camm/productions/bedwars/Items/SectionInventories/Inventories/ActionSelectionInventory.java
+++ src/main/java/me/camm/productions/bedwars/Items/SectionInventories/Inventories/ActionSelectionInventory.java
@@ -4,5 +4,7 @@ package me.camm.productions.bedwars.Items.SectionInventories.Inventories;
 /**
  * Unfinished.
  */
+//inventory for the cross road between tracker and comms inventories
+    //there should only be 1 instance of this.
 public class ActionSelectionInventory {
 }
