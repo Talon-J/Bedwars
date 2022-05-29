2e46fa9c9a3f3d6cac1fced5bc8f7f2f56466f8f
Talon J <talon.jiang@ryerson.ca>
2022-05-29 13:48:25 -0400

    Prototype for TNT velocity.

--- src/main/java/me/camm/productions/bedwars/Listeners/ExplosionHandler.java
+++ src/main/java/me/camm/productions/bedwars/Listeners/ExplosionHandler.java
@@ -7,7 +7,6 @@ import me.camm.productions.bedwars.Entities.ActiveEntities.Hierarchy.IGameTeamab
 import me.camm.productions.bedwars.Explosions.ExplosionParticle;
 import me.camm.productions.bedwars.Explosions.Vectors.ExplosionVector;
 import me.camm.productions.bedwars.Explosions.VectorToolBox;
-import me.camm.productions.bedwars.Explosions.Vectors.TracerVector;
 import me.camm.productions.bedwars.Explosions.VelocityComponent;
 import net.minecraft.server.v1_8_R3.DamageSource;
 import net.minecraft.server.v1_8_R3.Explosion;
@@ -29,10 +28,7 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 
 public class ExplosionHandler implements Listener
@@ -103,78 +99,70 @@ public class ExplosionHandler implements Listener
         ArrayList<Block> fireCandidates = new ArrayList<>();
 
         Location explosionCenter = exploded.getLocation();
-        double damageDistance = incendiary ? 6:8;
 
-
-        Collection<Entity> entities = explosionCenter.getWorld().getNearbyEntities(explosionCenter,damageDistance,damageDistance,damageDistance);
+        Collection<Entity> entities = explosionCenter.getWorld().getEntities();
 
         //now we calculate the damage for the entities first since we need to account for the blocks that might be
         //in the way protecting them.
 
 
         Vector origin = explosionCenter.toVector();
-                entities.forEach(entity -> {
 
-                    IS_LIVING:
-                    {
+        for (Entity entity : entities) {
+            if (!(entity instanceof LivingEntity)) {
+                continue;
+            }
 
-                        if (!(entity instanceof LivingEntity) || (!VectorToolBox.isValidDamageType(entity)))
-                            break IS_LIVING;
+            if (!VectorToolBox.isValidDamageType(entity)) {
+                continue;
+            }
 
-                        Vector location = entity.getLocation().toVector();
-                        Vector direction = location.clone().subtract(origin.clone());
-                        double length = direction.length();
+            Vector location = entity.getLocation().toVector();
+            Vector direction = location.clone().subtract(origin.clone());
+            double length = direction.length();
 
-                        TracerVector tracer = new TracerVector(direction.clone().normalize(), origin.clone(), length, world);
-                        ArrayList<Material> obstructions = tracer.getObstructionLayers();
+            if (entity.getLocation().distance(explosionCenter) > 8) {
+                continue;
+            }
 
-                            double damage = -0.25*((0.5*length)) +1;
+            double damage = -0.25 * ((0.5 * length)) + 1;
+            if (damage < 0)
+                damage = 0;
 
-                    //    damage *= -0.1 * (0.5*length + 0.3*obstructions.size()) + 1;  // this is a function
+            if (!VectorToolBox.isValidVelocityType(entity)) {
+                continue;
+            }
+
+            //unfinished. velocity needs reworking.
+            VelocityComponent component = new VelocityComponent(explodeEvent);
+            component.applyVelocity();
 
 
-                        if (damage < 0)
-                            damage = 0;
+            EntityDamageByEntityEvent event = new EntityDamageByEntityEvent(exploded, entity, EntityDamageEvent.DamageCause.ENTITY_EXPLOSION, damage);
+            Bukkit.getPluginManager().callEvent(event);
 
+            if (!event.isCancelled()) {
+                UUID id = entity.getUniqueId();
+                if (actionListener.contains(id)) {
 
-                        EntityDamageByEntityEvent event = new EntityDamageByEntityEvent(exploded, entity, EntityDamageEvent.DamageCause.ENTITY_EXPLOSION, damage);
-                        Bukkit.getPluginManager().callEvent(event);
+                    //accounting for enderdragons.
+                    IGameTeamable teamEntity = actionListener.getEntity(id);
+                    if (teamEntity instanceof GameDragon) {
+                        Explosion explosion = new Explosion(((CraftWorld) world).getHandle(), ((CraftEntity) exploded).getHandle(), location.getX(), location.getY(), location.getZ(), 0, false, false);
+                        ((GameDragon) teamEntity).dealRawDamage(DamageSource.explosion(explosion), (float) event.getFinalDamage());
+                    } else {
+                        ((LivingEntity) entity).damage(event.getFinalDamage(), exploded);
+                    }
+                    entity.setLastDamageCause(event);
+                    return;
+                }
 
-                        if (!event.isCancelled())
-                        {
-                            UUID id = entity.getUniqueId();
-                            if (actionListener.contains(id))
-                            {
+                if (arena.getPlayers().containsKey(entity.getUniqueId())) {
+                    ((LivingEntity) entity).damage(event.getFinalDamage(), exploded);
+                }
+            }
 
-                                //accounting for enderdragons.
-                              IGameTeamable teamEntity = actionListener.getEntity(id);
-                              if (teamEntity instanceof GameDragon)
-                              {
-                                  //Explosion(World world, Entity entity, double d0, double d1, double d2, float f, boolean flag, boolean flag1)
-                                  Explosion explosion = new Explosion(((CraftWorld)world).getHandle(),((CraftEntity)exploded).getHandle(),location.getX(), location.getY(), location.getZ(),0,false, false);
-                                  ((GameDragon)teamEntity).dealRawDamage(DamageSource.explosion(explosion),(float)event.getFinalDamage());
-                              }
-                              else
-                              {
-                                  ((LivingEntity)entity).damage(event.getFinalDamage(), exploded);
-                                  entity.setLastDamageCause(event);
-                              }
-                              return;
-                            }
-
-                            if (arena.getPlayers().containsKey(entity.getUniqueId())) {
-                                ((LivingEntity)entity).damage(event.getFinalDamage(), exploded);
-                            }
-                        }
-
-                        if (!VectorToolBox.isValidVelocityType(entity))
-                            break IS_LIVING;
-
-                        //unfinished. velocity needs reworking.
-                        VelocityComponent component = new VelocityComponent(explodeEvent);
-                        component.applyVelocity();
-           }
-        });
+        }
 
         double distance = 0;
 
@@ -187,7 +175,7 @@ public class ExplosionHandler implements Listener
                 boolean broken = directions.get(rays).conflict(block); //determine if the block should be broken.
 
                 if (!broken) {
-                    if (!directions.get(rays).validate())  //If the vector has lost power, then remove it to save resources.
+                    if (directions.get(rays).lostStrength())  //If the vector has lost power, then remove it to save resources.
                         directions.remove(rays);
                     continue;
                 }
@@ -197,7 +185,7 @@ public class ExplosionHandler implements Listener
                 if (incendiary)
                       fireCandidates.add(block);  //consider the block for fire [Might not be air]
 
-                if (!directions.get(rays).validate())  //If the vector has lost power, then remove it to save resources.
+                if (directions.get(rays).lostStrength())  //If the vector has lost power, then remove it to save resources.
                     directions.remove(rays);
 
             }//for
@@ -246,8 +234,8 @@ public class ExplosionHandler implements Listener
     //we should probably change this to be better. (I.e use final variables, make it more exact, such as a 90.5% chance instead of 90%)
     private void rollFireChance(Block block)
     {
-        int setFire = rand.nextInt(11);
-        if (setFire >= 9)
+        int setFire = rand.nextInt(101);
+        if (setFire >= 90)
             block.setType(Material.FIRE);
     }
 
@@ -267,15 +255,16 @@ public class ExplosionHandler implements Listener
         // we're doing this instead.
         for (double vertical=90;vertical>=-90;vertical-=5.625)
         {
-            double yComponent = Math.tan(vertical); //y value
+            double yComponent = Math.tan(Math.toRadians(vertical)); //y value
 
             //vectors going out in 360 degrees by intervals of 5.625 degrees
             //5.625 degrees is an arbitrary amount such that most, if not all blocks in a radius are hit by vectors similar
             //to regular tnt
             for (double horizontal=0;horizontal<=360;horizontal+=5.625)
             {
-                double xComponent = Math.sin(horizontal);  //x Value of the vector
-                double zComponent = Math.cos(horizontal);  //z Value of the vector
+                double horRads = Math.toRadians(horizontal);
+                double xComponent = Math.sin(horRads);  //x Value of the vector
+                double zComponent = Math.cos(horRads);  //z Value of the vector
 
                 directions.add(new ExplosionVector(new Vector(xComponent,yComponent,zComponent),
                         new Vector(xLocation,yLocation,zLocation),world,incendiary,colors));
